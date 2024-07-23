a2ba437be2bfac919243cc2dafac318b1af1d559
WesRamox <wesleyramox258@gmail.com>
2024-07-23 02:22:01 -0300

    Style updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Minus, Plus, ShoppingCart } from "lucide-react";
+import { Minus, Plus, ShoppingBag, ShoppingCart } from "lucide-react";
 import products from "./data/products";
 import { useState } from "react";
 import { useCart } from "./hooks/useCart";
@@ -30,31 +30,36 @@ function App() {
     <div>
       <div className="absolute right-2 top-2 z-10">
         <button className="" onClick={handleModalCart}>
-          <ShoppingCart />
+          <ShoppingCart size={20} />
+          {cart.length > 0 && (
+            <p className="bg-zinc-900 px-1 rounded absolute right-0 top-9 text-sm font-semibold">{totalItems}</p>
+          )}
         </button>
-        {cart.length > 0 && (
-          <p className="absolute right-2 top-5 font-semibold">{totalItems}</p>
-        )}
       </div>
 
-      <div className="flex gap-5 justify-center flex-wrap p-10">
+      <div className="flex gap-10 items-center justify-center flex-wrap p-10">
         {products.map((product) => (
-          <div key={product.id} className="w-fit">
-            <img src={product.image} alt={product.name} className="w-52 h-52 object-cover" />
+          <div key={product.id} className="w-fit justify-center flex flex-col">
+            <img src={product.image} alt={product.name} className="w-full h-52 object-cover" />
             <h2 className="text-xl text-gray-300 truncate w-52">{product.name}</h2>
             <p className="truncate w-52 text-gray-500">{product.description}</p>
             <p className="text-2xl my-1 font-semibold">R$ {product.price}</p>
-            <button onClick={() => addProductToCart(product)}>Adicionar ao carrinho</button>
+
+            <div className="buttons flex gap-2">
+              <button onClick={() => addProductToCart(product)}><ShoppingCart /></button>
+              <button className="flex gap-2 items-center justify-center"><ShoppingBag />Comprar agora</button>
+            </div>
           </div>
         ))}
       </div>
 
       {modalCart && (
-        <div className="absolute flex flex-col gap-2 top-10 right-0 bg-zinc-950 p-10 z-0 rounded">
+        <div className="absolute flex flex-col gap-2 top-10 right-0 bg-zinc-900 p-10 z-0 rounded">
           {cart.length > 0 ? (
-            <div>
+            <div className="flex flex-col gap-5">
               {cart.map((product) => (
                 <div className="flex flex-col" key={product.id}>
+                  <img src={product.image} alt={product.name} className="rounded-lg w-16 h-12 object-center" />
                   <h2 className="text-xl">{product.name}</h2>
                   <div className="flex gap-2 items-center">
                     <p>R$ {product.price}</p>
@@ -74,7 +79,7 @@ function App() {
                   </div>
                 </div>
               ))}
-              <p>Total: R$ {total.toFixed(2)}</p>
+              <p className="text-xl">Total: R$ {total.toFixed(2)}</p>
             </div>
           ) : (
             <p>Nenhum produto no carrinho</p>
--- src/data/products.ts
+++ src/data/products.ts
@@ -13,7 +13,7 @@ const products: ProductData[] =  [
         name: "Fone de Ouvido Bluetooth",
         description: "Fone de ouvido sem fio com cancelamento de ruído e bateria de longa duração.",
         price: 199.99,
-        image: "https://i5.walmartimages.com/asr/dda6bc1f-d282-4cf9-ad29-e827222bc4d5.8d402328f4d54e2b9a252879ec51fb79.jpeg",
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTHiEYsu_OqnYGS71FYgEg5wj_an5jcS7AEIMKsPUJputsHbg9ndq2fSM8t4jp7i78hMj8Diu6gMdhhZN-Iw5znZwNDbIclXPnUW-lv6rUtxRIwzSsQlYPD&usqp=CAE",
         quantity: 0
     },
     {
@@ -21,7 +21,7 @@ const products: ProductData[] =  [
         name: "Teclado Mecânico RGB",
         description: "Teclado mecânico com iluminação RGB personalizável e teclas programáveis.",
         price: 299.99,
-        image: "https://i5.walmartimages.com/asr/dda6bc1f-d282-4cf9-ad29-e827222bc4d5.8d402328f4d54e2b9a252879ec51fb79.jpeg",
+        image: "https://www.syma.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/3/4/34529.jpg",
         quantity: 0
     },
     {
@@ -45,7 +45,7 @@ const products: ProductData[] =  [
         name: "Mouse Gamer",
         description: "Mouse gamer com alta precisão, botões programáveis e iluminação LED.",
         price: 79.99,
-        image: "https://m.media-amazon.com/images/I/61mpMH5TzkL._AC_SL1500_.jpg",
+        image: "https://aocstore.vtexassets.com/arquivos/ids/158657-800-800?v=638278206409270000&width=800&height=800&aspect=true",
         quantity: 0
     }
 ];
